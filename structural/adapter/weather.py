efd75a41a966686911004eef2cc51a6971458fe5
aver <froloff92@mail.ru>
2025-03-26 15:15:42 +0300

    fixes for adapter

--- structural/adapter/weather.py
+++ structural/adapter/weather.py
@@ -2,7 +2,7 @@ class YahooWeather:
     def request_weather(self, city):
         return {"city": city, "temp": "+25°C"}
 
-class OpenWeatherMap:
+class OpenWeather:
     def get_weather(self, location):
         return {"location": location, "temperature": "25°C"}
 
@@ -15,24 +15,10 @@ class WeatherAdapter:
         return {"location": data['city'], "temperature": data['temp'][1:]}
 
 
-
-class YahooWeatherService:
-    def get_temp(self, city):
-        return f"Температура в {city}: +25°C"
-
-class OpenWeatherService:
-    def fetch_weather(self, location):
-        return {"location": location, "temp": "25°C"}
-
-
-class WeatherAdapterService(YahooWeatherService, OpenWeatherService):
+class WeatherAdapterService(YahooWeather, OpenWeather):
     def get_weather(self, city):
-        # Если нужно, можно выбрать, какой метод вызывать
-        if len(city) < 10:  # Условная логика
-            return self.get_temp(city)
-        else:
-            data = self.fetch_weather(city)
-            return f"Температура: {data['temp']}"
+        data = self.request_weather(city)
+        return {"location": data['city'], "temperature": data['temp'][1:]}
 
 
 def main():
@@ -44,7 +30,6 @@ def main():
     # Использование метода наследования 
     adapter = WeatherAdapterService()
     print(adapter.get_weather("Москва"))      # "Температура в Москва: +25°C"
-    print(adapter.get_weather("Санкт-Петербург"))  # "Температура: 25°C"
 
 if __name__ == '__main__':
     main()
